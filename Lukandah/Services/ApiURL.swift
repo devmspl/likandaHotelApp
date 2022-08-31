5da8a108b273fd5860e2c950920ca6525a2b1fc9
devmspl <developer.mspl@gmail.com>
2022-08-31 12:28:16 +0530

    LatestChanges

--- Lukandah/Services/ApiURL.swift
+++ Lukandah/Services/ApiURL.swift
@@ -21,5 +21,9 @@ public struct Api {
     public static var SearchHotels = baseUrl + "hotels/search?name="
     public static var RatingReviews = baseUrl + "hotels/ratingReview"
     public static var Hotelsbyfilter = baseUrl + "hotels/byFilter?"
+    public static var UserLogout = baseUrl + "users/logout"
+    public static var UserOTP = baseUrl + "users/otp?email="
+    public static var Otpverify = baseUrl + "users/otpVerify"
+    public static var Forgotpassword = baseUrl + "users/forgotPassword"
 }
  
--- Lukandah/ViewControllers/OnBoarding/ResetPassVC.swift
+++ Lukandah/ViewControllers/OnBoarding/ResetPassVC.swift
@@ -6,20 +6,57 @@
 //
 
 import UIKit
+import Alamofire
 
 class ResetPassVC: UIViewController {
-
+    @IBOutlet weak var Emailtext: UITextField!
+    @IBOutlet weak var Newpasswordtext: UITextField!
+    @IBOutlet weak var Confirmpasswordtext: UITextField!
+    
+    var data = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
     
     
     @IBAction func continue_tapped(_ sender: Any) {
-        let vc = self.storyboard?.instantiateViewController(withIdentifier: "LoginVC") as! LoginVC
-        self.navigationController?.pushViewController(vc, animated: true)
+        forgotpasswordApi()
     }
     
+}
+
+extension ResetPassVC{
+    func forgotpasswordApi(){
+        let token = UserDefaults.standard.object(forKey: "token") as! String
+        let para:[String:Any] = [
+            "email":Emailtext.text!,
+            "newPassword":Newpasswordtext.text!,
+            "otpToken":token
+        ]
+        print("FORGOTPASSWORD",para)
+        AF.request(Api.Forgotpassword,method: .post,parameters: para,encoding: JSONEncoding.default).responseJSON{
+            response in
+            switch(response.result){
+            case .success(let json):do{
+                let status = response.response?.statusCode
+                let respond = json as! NSDictionary
+                print(respond)
+                if status == 200{
+                    self.data = respond.object(forKey: "message") as! String
+                    
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "LoginVC") as! LoginVC
+                    self.navigationController?.pushViewController(vc, animated: true)
+            }
+          
+        }
+            case .failure(let error): do{
+                print(error)
+            }
+}
+}
+}
 }
